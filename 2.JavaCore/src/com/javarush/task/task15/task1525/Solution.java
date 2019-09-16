762b9d7a8e6645efb0fd2c84fc1887b69d028208
ekaterina.volkova <ekaterina.volkova@ocrv.ru>
2019-09-16 17:28:43 +0300

    05.25

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task15.task1525;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/* 
+Файл в статическом блоке
+*/
+
+public class Solution {
+    public static List<String> lines = new ArrayList<String>();
+    static {
+        try {
+            File file = new File(Statics.FILE_NAME);
+            FileReader fileReader = new FileReader(file); // поток который подключается к текстовому файлу
+            BufferedReader bufferedReader = new BufferedReader(fileReader); // соединяем FileReader с BufferedReader
+
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+//                System.out.println(line); // выводим содержимое файла на экран построчно
+                lines.add(line);
+            }
+
+            bufferedReader.close(); // закрываем поток
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lines);
+    }
+}
